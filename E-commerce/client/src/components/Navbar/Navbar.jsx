2c9575111c91f3841645bb865aaab68fdca12a7f
Freeze-ssp <92584308+Freeze-ssp@users.noreply.github.com>
2023-04-09 13:15:03 +0530

    Update Navbar.jsx

--- E-commerce/client/src/components/Navbar/Navbar.jsx
+++ E-commerce/client/src/components/Navbar/Navbar.jsx
@@ -23,7 +23,7 @@ const Navbar = () => {
                       <KeyboardArrowDownIcon />
                     </div>
                     <div className="item">
-                      <Link className="link" to="/products/1">Bedsheets</Link> 
+                      <Link className="link" to="/products/7">Bedding</Link> 
                     </div>
                     <div className="item">
                       <Link className="link" to="/products/3">Women</Link>
